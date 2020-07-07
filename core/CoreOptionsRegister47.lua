4ee6535a23f2e41493fe25fbf18b31ceb83bb60e
Todd Nguyen <todd.nguyen47@gmail.com>
2020-07-07 16:19:56 -0700

    Add specific disable options for "Profiles" tab

--- core/CoreOptionsRegister47.lua
+++ core/CoreOptionsRegister47.lua
@@ -1,7 +1,9 @@
 local ZxStartingGold = LibStub("AceAddon-3.0"):GetAddon("ZxStartingGold")
-local Utils47 = ZxStartingGold.Utils47
----@type CoreFactory47
-local CoreFactory47 = ZxStartingGold.CoreFactory47
+---@type ZxStartingGold
+local MainAddon = ZxStartingGold
+
+local Utils47 = MainAddon.Utils47
+local FramePool47 = MainAddon.FramePool47
 
 local AceConfigRegistry = LibStub("AceConfigRegistry-3.0")
 local AceConfigDialog = LibStub("AceConfigDialog-3.0")
@@ -9,14 +11,14 @@ local AceGUI = LibStub("AceGUI-3.0")
 local AceDBOptions = LibStub("AceDBOptions-3.0")
 
 ---@class CoreOptionsRegister47
-local CoreOptionsRegister47 = ZxStartingGold:NewModule("Options", nil)
+local CoreOptionsRegister47 = MainAddon:NewModule("Options", nil)
 
 -- PRIVATE functions and variables
 ---@param key string
-local _OPEN_OPTION_APPNAME = "ZxStartingGold_OpenOption"
+local _OPEN_OPTION_APPNAME = MainAddon.ADDON_NAME .. "_OpenOption"
 
 function CoreOptionsRegister47:OnInitialize()
-  self._curDbProfile = ZxStartingGold.db.profile
+  self._curDbProfile = MainAddon.db.profile
   self._openOptionTable = {}
   self._options = {}
   self._printFrameOptionTable = {}
@@ -25,31 +27,35 @@ function CoreOptionsRegister47:OnInitialize()
 end
 
 function CoreOptionsRegister47:SetupOptions()
-  ZxStartingGold.blizOptionTable = {}
+  MainAddon.blizOptionTable = {}
   AceConfigRegistry:RegisterOptionsTable(_OPEN_OPTION_APPNAME,
     function(...) return self:_getOpenOptionTable() end)
-  AceConfigRegistry:RegisterOptionsTable(ZxStartingGold.ADDON_NAME,
+  AceConfigRegistry:RegisterOptionsTable(MainAddon.ADDON_NAME,
     function(...) return self:_getOptionTable() end)
 
   local frameRef = AceConfigDialog:AddToBlizOptions(_OPEN_OPTION_APPNAME,
-                     ZxStartingGold.DECORATIVE_NAME)
-  ZxStartingGold.blizOptionTable[ZxStartingGold.ADDON_NAME] = frameRef
+                     MainAddon.DECORATIVE_NAME)
+  MainAddon.blizOptionTable[MainAddon.ADDON_NAME] = frameRef
   -- Register slash commands as well
-  for _, command in pairs(ZxStartingGold.SLASH_COMMANDS) do
-    ZxStartingGold:RegisterChatCommand(command, function(...) self:_openOptionFrame() end)
+  for _, command in pairs(MainAddon.SLASH_COMMANDS) do
+    MainAddon:RegisterChatCommand(command, function(...) self:_openOptionFrame() end)
   end
 
   -- Set profile options
-  local profileTable = AceDBOptions:GetOptionsTable(ZxStartingGold.db)
+  local profileTable = AceDBOptions:GetOptionsTable(MainAddon.db)
+  profileTable.args.reset["disabled"] = false
   local moduleName = "Profiles"
-  CoreFactory47:disableEnabledToggleInCombat(moduleName, profileTable["args"])
-  ZxStartingGold:registerModuleOptions(moduleName, profileTable, moduleName)
+  MainAddon:registerModuleOptions(moduleName, profileTable, moduleName)
 
   -- Set Print Frames option
   moduleName = "PrintFrames"
   local printFrameTable = self:_getPrintFrameOptionTable()
-  CoreFactory47:disableEnabledToggleInCombat(moduleName, printFrameTable["args"])
-  ZxStartingGold:registerModuleOptions(moduleName, printFrameTable, "Print Frames")
+  MainAddon:registerModuleOptions(moduleName, printFrameTable, "Print Frames")
+
+  -- Disable all modules when combat starts
+  -- Resume its previous disabled-state when combat ends
+  local disableInCombat = self.DisableInCombat:new()
+  disableInCombat:disableOptions()
 end
 
 -- ########################################
@@ -95,7 +101,7 @@ function CoreOptionsRegister47:_getPrintFrameOptionTable()
             local sortedKeys = {}
             local t1 = {}
             local s1 = "FRAMES VISIBILITY\n"
-            for k, v in ZxStartingGold:IterateModules() do
+            for k, v in MainAddon:IterateModules() do
               table.insert(sortedKeys, k)
               t1[k] = v
             end
@@ -123,9 +129,7 @@ end
 
 function CoreOptionsRegister47:_getSlashCommandsString()
   local s1 = "You can also open the options frame with one of these commands:\n"
-  for _, command in pairs(ZxStartingGold.SLASH_COMMANDS) do
-    s1 = s1 .. "    /" .. command .. "\n"
-  end
+  for _, command in pairs(MainAddon.SLASH_COMMANDS) do s1 = s1 .. "    /" .. command .. "\n" end
   s1 = string.sub(s1, 0, string.len(s1) - 1)
   return s1
 end
@@ -139,8 +143,8 @@ function CoreOptionsRegister47:_openOptionFrame(info, value, ...)
       self._isFrameClosed = true
       AceGUI:Release(widget)
     end)
-    frame:SetTitle(ZxStartingGold.DECORATIVE_NAME)
-    AceConfigDialog:Open(ZxStartingGold.ADDON_NAME, frame)
+    frame:SetTitle(MainAddon.DECORATIVE_NAME)
+    AceConfigDialog:Open(MainAddon.ADDON_NAME, frame)
   end
 end
 
@@ -149,14 +153,15 @@ function CoreOptionsRegister47:_getOptionTable()
     self._options = {type = "group", args = {}}
     self:_addModuleOptionTables()
   end
+
   return self._options
 end
 
 function CoreOptionsRegister47:_addModuleOptionTables()
   local defaultOrderIndex = 7
-  table.sort(ZxStartingGold.moduleKeySorted)
-  for _, moduleAppName in pairs(ZxStartingGold.moduleKeySorted) do
-    local optionTableOrFunc = ZxStartingGold.moduleOptionsTable[moduleAppName]
+  table.sort(MainAddon.moduleKeySorted)
+  for _, moduleAppName in pairs(MainAddon.moduleKeySorted) do
+    local optionTableOrFunc = MainAddon.moduleOptionsTable[moduleAppName]
     if type(optionTableOrFunc) == "function" then
       self._options.args[moduleAppName] = optionTableOrFunc()
     else
@@ -172,6 +177,57 @@ function CoreOptionsRegister47:_addModuleOptionTables()
     else
       self._options.args[moduleAppName]["order"] = defaultOrderIndex
       defaultOrderIndex = defaultOrderIndex + 1
+    end
+  end
+end
+
+---@class DisableInCombat
+CoreOptionsRegister47.DisableInCombat = {}
+CoreOptionsRegister47.DisableInCombat.__index = CoreOptionsRegister47.DisableInCombat
+
+---@return DisableInCombat
+function CoreOptionsRegister47.DisableInCombat:new()
+  ---@type DisableInCombat
+  local newInstance = setmetatable({}, self)
+  newInstance._prevDisableOptions = {}
+  return newInstance
+end
+
+function CoreOptionsRegister47.DisableInCombat:disableOptions()
+  local frame = FramePool47:getFrame()
+  frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+  frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+  frame:SetScript("OnEvent", function(curFrame, event, arg1, arg2, ...)
+    if event == "PLAYER_REGEN_DISABLED" then
+      self:_disableInCombat()
+    elseif event == "PLAYER_REGEN_ENABLED" then
+      self:_enableOutOfCombat()
+    end
+  end)
+end
+
+function CoreOptionsRegister47.DisableInCombat:_disableInCombat()
+  for key, val in pairs(MainAddon.moduleOptionsTable) do
+    if key:find("Profile") then
+      val["args"]["reset"]["disabled"] = true
+    else
+      self._prevDisableOptions[key] = {}
+      for argkey, argval in pairs(val.args) do
+        self._prevDisableOptions[key][argkey] = argval["disabled"]
+        argval["disabled"] = true
+      end
+    end
+  end
+end
+
+function CoreOptionsRegister47.DisableInCombat:_enableOutOfCombat()
+  for key, val in pairs(MainAddon.moduleOptionsTable) do
+    if key:find("Profile") then
+      val["args"]["reset"]["disabled"] = nil
+    else
+      for argkey, argval in pairs(val.args) do
+        argval["disabled"] = self._prevDisableOptions[key][argkey]
+      end
     end
   end
 end
